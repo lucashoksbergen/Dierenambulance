37d7bda329277bcb7aad42f2be648e4ca0b4e3f5
afonsoocamacho <afonsocamacho62@gmail.com>
2025-05-06 13:46:04 +0200

    Refactor login view to use Blade components and sections for improved structure

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,16 @@
-<x-layout>Login<x-layout>
+@extends('layout')
+
+@section('title', 'DierenAmbulance - Welcome')
+
+@section('content')
+ <h1>Login</h1>
+@endsection
+
+@push('styles')
+  <!-- Page-specific CSS -->
+  <!-- <link rel="stylesheet" href="{{ asset('css/welcome.css') }}"> -->
+@endpush
+
+@push('scripts')
+  <!-- Page-specific JS if needed -->
+@endpush
