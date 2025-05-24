5339d084780de66408c9c965dd7d9698ac1c10be
afonsoocamacho <afonsocamacho62@gmail.com>
2025-05-24 14:56:07 +0200

    Add authentication routes, add protected routes middleware, and create dashboard view

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+})->name('home'); 
 
+// Authentication routes
 Route::get('/login',[AuthController::class, 'showLogin'] )->name('show.login');
-
 Route::post('/login',[AuthController::class, 'login'] )->name('login');
-
 Route::post('/logout',[AuthController::class, 'logout'] )->name('logout');
+
+//Protected routes
+Route::middleware('auth')->group(function(){
+
+    // Protected routes go here
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+
+});
+
 
 //TESTING HERE v ----------------
 
