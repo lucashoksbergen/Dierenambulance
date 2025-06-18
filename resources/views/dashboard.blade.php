25e22f5b29c8925f65a85ad9b581b9162a54ba19
liamkramer <liamkramer005@gmail.com>
2025-06-18 13:51:16 +0200

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,33 @@
 @section('title', 'DierenAmbulance - Dashboard')
 
 @section('content')
- <h1>dashboard</h1>
+
+<h1>Dashboard</h1>
+
+<div>
+  <div>
+    <div>
+      <div>#0000</div><div>-</div><div>Taxirit/Zwerfdier</div>
+    </div>
+    <div>Status</div>
+  </div>
+  <div>
+    <div>
+      <div class="information"><div>Diersoort:</div><div>Diersoort</div></div>
+      <div class="information"><div>Datum:</div><div>00/00/00 @ 00:00</div></div>
+    </div>
+    <div>
+      <div class="information"><div>Locatie:</div><div>Locatie</div></div>
+      <div class="information"><div>Gemeente:</div><div>Gemeente</div></div>
+    </div>
+    <div>
+      <div class="information"><div>Bestuurder:</div><div>Bestuurder</div></div>
+      <div class="information"><div>Bijrijder:</div><div>Bijrijder</div></div>
+    </div>
+  </div>
+</div>
+
+
 @endsection
 
 @push('styles')
